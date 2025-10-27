8216007d91365576f2b850143a42e4a588cb0eb6
Arlind Shaqiri <shaqirivarlind@gmail.com>
2025-10-27 17:34:02 +0100

    init pipelines

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,9 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {CodePipeline, ShellStep, CodePipelineSource} from 'aws-cdk-lib/pipelines'
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'AwesomePipeline', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('shaqirivarlind/cdk-cicd', 'cicd-practice'),
+        commands: [
+          // 'cd cdk-cicd', // this is the path in case if you have multiple project in one root
+          'npm ci',
+          'npx cdk synth'
+        ],
+        // primaryOutputDirectory: 'cdk-cicd/cdk.out', // this is the path in case if you have multiple project in one root
+      })
+    })
 
   }
 }
